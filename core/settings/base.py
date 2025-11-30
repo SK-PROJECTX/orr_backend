48d4a6203ae1c0dcadc37fe0e130a4b8bbe0bd04
olamide <olamoyegunolamidesammy@gmail.com>
2025-11-30 16:48:50 +0100

    add allowhost

--- core/settings/base.py
+++ core/settings/base.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['orr-backend-web-latest.onrender.com','localhost','127.0.0.1', 'orr-backend-web-latest.onrender.com']
 
 # Application definition
 
