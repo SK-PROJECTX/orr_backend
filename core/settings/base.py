26fd3f80bf4331952840f5ad867c988d048457d7
olamide <olamoyegunolamidesammy@gmail.com>
2025-11-30 15:15:23 +0100

    .

--- core/settings/base.py
+++ core/settings/base.py
@@ -27,8 +27,11 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="").split(",")
-
+ALLOWED_HOSTS = [
+    "orr-backend-web-latest.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
