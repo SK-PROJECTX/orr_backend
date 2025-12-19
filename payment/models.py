5f1fa79df05c943befa8752fe1d0bc091d4db525
olamide <olamoyegunolamidesammy@gmail.com>
2025-12-19 03:06:50 +0100

    Alter StripeCustomer model to allow nullable and blank stripe_card_id field

--- payment/models.py
+++ payment/models.py
@@ -76,11 +76,10 @@ class Invoice(Audit):
     def __str__(self):
         return f"Invoice {self.stripe_invoice_id}"
 
-
 class StripeCustomer(Audit):
-    user = models.OneToOneField(User, on_delete=models.CASCADE , related_name="stripe_profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="stripe_profile")
     stripe_customer_id = models.CharField(max_length=255, unique=True)
-    stripe_card_id = models.CharField(max_length=255, unique=True)
+    stripe_card_id = models.CharField(max_length=255, blank=True, null=True)  
 
     def __str__(self):
         return f"{self.user.username} Stripe Customer"
