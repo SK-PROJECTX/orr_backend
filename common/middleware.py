9b5cdde03b3fe01297812679e408f0d409326356
Abdullateef2006 <ojugbelelateef2006@gmail.com>
2026-04-19 02:05:47 +0100

    added italian translation

---
+++ common/middleware.py
@@ -0,0 +1,27 @@
+from django.utils import translation
+from django.conf import settings
+
+class QueryParameterLocaleMiddleware:
+    """
+    Middleware that sets the language based on a 'lang' query parameter.
+    This is useful for API requests from frontends that manage language state.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        language = request.GET.get('lang')
+        
+        # Check if language is supported
+        supported_languages = [lang[0] for lang in settings.LANGUAGES]
+        
+        if language and language in supported_languages:
+            translation.activate(language)
+            request.LANGUAGE_CODE = translation.get_language()
+        
+        response = self.get_response(request)
+        
+        # Deactivate translation after request
+        translation.deactivate()
+        
+        return response
