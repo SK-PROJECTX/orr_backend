6da2288e4845e2851890ab91742f825838ce41a5
Abdullateef2006 <ojugbelelateef2006@gmail.com>
2026-05-05 15:04:58 +0100

    added social login

--- common/urls.py
+++ common/urls.py
@@ -3,10 +3,11 @@ from .views import PublicHomepageView
 
 from .views import CurrentUserRoleView
 
-from .auth_views import LoginView
+from .auth_views import LoginView, GoogleLoginView
 
 urlpatterns = [
     path("auth/me/", CurrentUserRoleView.as_view(), name="current-user-role"),
     path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/google-login/", GoogleLoginView.as_view(), name="google-login"),
     path("cms/homepage/", PublicHomepageView.as_view(), name="public-homepage"),
 ]
